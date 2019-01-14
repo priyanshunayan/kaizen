d335ece0992d238f1134ad0f9f4f7c92f4179ad4
Priyanshu Nayan <priyanshunayan456@gmail.com>
2019-01-14 14:39:15 +0530

    add reactive forms

--- kaizen-front/src/app/authentication/authentication.module.ts
+++ kaizen-front/src/app/authentication/authentication.module.ts
@@ -4,11 +4,13 @@ import { CommonModule } from '@angular/common';
 import { AuthenticationRoutingModule } from './authentication-routing.module';
 import { LoginComponent } from './login/login.component';
 import { SignupComponent } from './signup/signup.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   imports: [
     CommonModule,
-    AuthenticationRoutingModule
+    AuthenticationRoutingModule,
+    ReactiveFormsModule
   ],
   declarations: [LoginComponent, SignupComponent]
 })
--- kaizen-front/src/app/authentication/signup/signup.component.ts
+++ kaizen-front/src/app/authentication/signup/signup.component.ts
@@ -1,14 +1,26 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormGroup, FormControl,  FormBuilder, Validators } from '@angular/forms';
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
   styleUrls: ['./signup.component.css']
 })
 export class SignupComponent implements OnInit {
-
-  constructor() { }
-
+/*   profileForm = new FormGroup({
+    firstName: new FormControl(''),
+    lastName: new FormControl(''),
+  }); */
+  profileForm = this.fb.group({
+    firstName: ['', Validators.required],
+    lastName: [''],
+    email: [''],
+    password: ['']
+  });
+  onSubmit() {
+    // TODO: Use EventEmitter with form value
+    console.warn(this.profileForm.value);
+  }
+  constructor(private fb: FormBuilder) {}
   ngOnInit() {
   }
 
