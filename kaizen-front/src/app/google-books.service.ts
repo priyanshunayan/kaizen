ace8ee4595271bc4b65689791ea4d57c3e75d494
Priyanshu Nayan <priyanshunayan456@gmail.com>
2019-08-11 15:18:35 +0530

    Add read, toread, and favourite api

--- kaizen-front/src/app/google-books.service.ts
+++ kaizen-front/src/app/google-books.service.ts
@@ -8,10 +8,28 @@ export class GoogleBooksService {
 
   constructor(private http: HttpClient) { }
   getBooks(key) {
-   return this.http.get(`https://www.googleapis.com/books/v1/volumes?q=${key}`);
+    return this.http.get(`https://www.googleapis.com/books/v1/volumes?q=${key}`);
   }
   getSpecificBook(id) {
     return this.http.get(`https://www.googleapis.com/books/v1/volumes/${id}`);
+  }
+  toRead(userId, book) {
+    return this.http.post(`http://localhost:3000/toread`, {
+      'userId': userId,
+      'toread': book
+    });
+  }
+  read(userId, book) {
+    return this.http.post(`http://localhost:3000/read`, {
+      'userId': userId,
+      'read': book
+    });
+  }
+  favourite(userId, book) {
+    return this.http.post(`http://localhost:3000/favourite`, {
+      'userId': userId,
+      'favourite': book
+    });
   }
 }
 
--- kaizen-front/src/app/user/book-specific/book-specific.component.ts
+++ kaizen-front/src/app/user/book-specific/book-specific.component.ts
@@ -17,9 +17,41 @@ export class BookSpecificComponent implements OnInit {
   getBookDetails() {
     const id = this.route.snapshot.paramMap.get('id');
     this.bookService.getSpecificBook(id).subscribe(res => {
-      console.log("Book", res);
+      // console.log('Book', res);
       this.bookDetails = (<any>res).volumeInfo;
     });
   }
-
+  toread(event) {
+    const data = localStorage.getItem('Kaizen');
+    const json = JSON.parse(data);
+    console.log(json.user);
+    const user_id = json.user._id;
+    this.bookService.toRead(user_id, this.bookDetails).subscribe(res => {
+      console.log('Added to To read successfully', res);
+    }, (e) => {
+      console.log(e);
+    });
+  }
+  read(event) {
+    const data = localStorage.getItem('Kaizen');
+    const json = JSON.parse(data);
+    console.log(json.user);
+    const user_id = json.user._id;
+    this.bookService.read(user_id, this.bookDetails).subscribe(res => {
+      console.log('Added successfully');
+    }, (e) => {
+      console.log(e);
+    });
+  }
+  favourite(event) {
+    const data = localStorage.getItem('Kaizen');
+    const json = JSON.parse(data);
+    console.log(json.user);
+    const user_id = json.user._id;
+    this.bookService.favourite(user_id, this.bookDetails).subscribe(res => {
+      console.log('Added successfully');
+    }, (e) => {
+      console.log(e);
+    });
+  }
 }
